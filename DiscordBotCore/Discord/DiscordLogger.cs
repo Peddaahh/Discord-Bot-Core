96c58f3b63dd5fff7972c7bbf6bc20694e72336c
Petr Sedláček <petr.spelos@gmail.com>
2018-06-13 11:55:46 +0200

    Implemented basic Discord Connection

---
+++ DiscordBotCore/Discord/DiscordLogger.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using Discord;
+
+namespace DiscordBotCore.Discord
+{
+    public class DiscordLogger
+    {
+        ILogger _logger;
+
+        public DiscordLogger(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Log(LogMessage logMsg)
+        {
+            _logger.Log(logMsg.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
